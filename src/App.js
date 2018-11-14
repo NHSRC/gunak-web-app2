6aff9c846c3a486d9c4653b8929ad77147cadeca
Vivek Singh <petmongrels@gmail.com>
2018-11-14 18:34:05 +0530

    List view customised

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import dataProvider from './framework/gunak-data-provider';
 import authProvider from './framework/auth-provider';
+import {AssessmentToolModeList} from './View/AssessmentToolMode';
 
 const App = () => (
     <Admin dataProvider={dataProvider('/api')} authProvider={authProvider}>
-        <Resource name="assessmentToolMode" list={ListGuesser} />
+        <Resource name="assessmentToolMode" list={AssessmentToolModeList} />
         <Resource name="assessmentTool" list={ListGuesser} />
     </Admin>
 );
