8605e027286a9ecfdc033a569e60d99fee4c8042
Vivek Singh <petmongrels@gmail.com>
2019-01-23 18:17:07 +0530

    Display state name in the checklist name only when state is not selected

--- src/View/Checklist.js
+++ src/View/Checklist.js
@@ -19,13 +19,20 @@ import {
 } from 'react-admin';
 import {GunakReferenceInput} from "../components/Inputs";
 import AppConfiguration from "../framework/AppConfiguration";
+import _ from 'lodash';
+
+let currentFilter = {};
 
 const EntityFilter = (props) => (
     <Filter {...props}>
-        {AppConfiguration.isJSS() ? <ReferenceInput label="State" source="stateId" reference="state" alwaysOn perPage={100} sort={{field: 'name', order: 'ASC'}}>
+        {AppConfiguration.isJSS() ? <ReferenceInput label="State" source="stateId" reference="state" alwaysOn perPage={100} sort={{field: 'name', order: 'ASC'}}
+                                                    onChange={(obj, id) => {
+                                                        currentFilter.stateId = id;
+                                                    }}>
             <SelectInput optionText="name"/>
         </ReferenceInput> : null}
-        <ReferenceInput label="Assessment tool" source="assessmentToolId" reference="assessmentTool" alwaysOn perPage={100} sort={[{field: 'id', order: 'ASC'}, {field: 'name', order: 'ASC'}]}>
+        <ReferenceInput label="Assessment tool" source="assessmentToolId" reference="assessmentTool" alwaysOn perPage={100}
+                        sort={[{field: 'id', order: 'ASC'}, {field: 'name', order: 'ASC'}]}>
             <SelectInput optionText="fullName"/>
         </ReferenceInput>
     </Filter>
@@ -35,7 +42,7 @@ export const ChecklistList = props => (
     <div>
         <List {...props} title='Checklists' filters={<EntityFilter/>} perPage={25} sort={{field: 'name', order: 'ASC'}}>
             <Datagrid>
-                <TextField source={AppConfiguration.isNHSRC() ? "name" : "fullName"}/>
+                <TextField source={(AppConfiguration.isNHSRC() || (AppConfiguration.isJSS() && !_.isNil(currentFilter.stateId) && !_.isEmpty(currentFilter.stateId))) ? "name" : "fullName"}/>
                 <ReferenceField label="Department" source="departmentId" reference="department" sortBy="department.name">
                     <TextField source="name"/>
                 </ReferenceField>
