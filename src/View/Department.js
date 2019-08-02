fc08822c07ab12100afa41c83a8df6289d538af5
Vivek Singh <petmongrels@gmail.com>
2019-08-02 15:07:42 +0530

    Extracted create/edit form in a single method

--- src/View/Department.js
+++ src/View/Department.js
@@ -3,30 +3,32 @@ import {BooleanField, BooleanInput, Create, Datagrid, DisabledInput, Edit, EditB
 import Privileges from "../model/Privileges";
 
 export const DepartmentList = ({privileges, ...props}) => (
-    <List {...props} title='Departments' sort={{ field: 'name', order: 'ASC' }} perPage={25}>
+    <List {...props} title='Departments' sort={{field: 'name', order: 'ASC'}} perPage={25}>
         <Datagrid rowClick="edit">
-            <TextField source="name" />
+            <TextField source="name"/>
             <BooleanField source="inactive"/>
-            <TextField source="id" />
+            <TextField source="id"/>
             {Privileges.hasPrivilege(privileges, 'Facility_Metadata_Write') && <EditButton/>}
         </Datagrid>
     </List>
 );
 
+const getForm = function (props, isEdit) {
+    return <SimpleForm>
+        {isEdit && <DisabledInput source="id"/>}
+        <TextInput source="name"/>
+        <BooleanInput source="inactive" defaultValue={false}/>
+    </SimpleForm>;
+};
+
 export const DepartmentEdit = props => (
     <Edit {...props} undoable={false}>
-        <SimpleForm>
-            <DisabledInput source="id" />
-            <TextInput source="name" />
-        </SimpleForm>
+        {getForm(props, true)}
     </Edit>
 );
 
 export const DepartmentCreate = (props) => (
     <Create {...props}>
-        <SimpleForm>
-            <TextInput source="name" />
-            <BooleanInput source="inactive" defaultValue={false}/>
-        </SimpleForm>
+        {getForm(props, false)}
     </Create>
 );
